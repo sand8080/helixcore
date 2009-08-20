dc2431246267592b457efa099600ca9ee7248bb7
Alexander Kislitsky <sand8080@gmail.com>
2009-08-20 17:21:33 +0400

    Test db user chahged to helixtest.

--- src/helixcore/test/test_environment.py
+++ src/helixcore/test/test_environment.py
@@ -2,6 +2,6 @@ import psycopg2
 from helixcore.db import wrapper
 from functools import partial
 
-DSN = 'dbname=helixtest host=localhost user=helixbilling password=qazwsx'
+DSN = 'dbname=helixtest host=localhost user=helixtest password=qazwsx'
 get_connection = partial(psycopg2.connect, DSN)
 transaction = partial(wrapper.transaction, get_connection)
