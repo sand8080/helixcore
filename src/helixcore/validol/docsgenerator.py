62595b33c49da97904a472c0ae1327c3cdc7e478
smee <om@webdrive.ru>
2009-10-26 20:04:25 +0300

    docstring formatter for docs generation

--- src/helixcore/validol/docsgenerator.py
+++ src/helixcore/validol/docsgenerator.py
@@ -2,131 +2,213 @@ from validol import AnyOf, Text
 from helixcore.validol.validol import Optional, Positive
 import os
 
-def block_brace(s):
-    return '<div class="brace">%s</div>' % s
+class AbstractFormatter(object):
+    def format(self, data, nested_hash=False, add_line_if_not_exist=None):
+        if isinstance(data, basestring):
+            return self.string(data)
 
-def inline_brace(s):
-    return '<span class="brace">%s</span>' % s
+        if isinstance(data, AnyOf):
+            return self.OR(data.validators, add_line_if_not_exist)
 
-def string(s):
-    return '<span class="string">"%s"</span>' % s
+        if isinstance(data, type):
+            if data == bool:
+                return self.choice_values(['true', 'false'])
+            if data == int:
+                return self.atomic_type(safe('integer'))
+            else:
+                return self.atomic_type(safe('!!!! type %s !!!' % data))
 
-def atomic_type(s):
-    return '<span class="variable">%s</span>' % s
+        if isinstance(data, list):
+            assert len(data) == 1
+            return self.list_type(self.format(data[0]))
 
-def list_type(s):
-    return '<span class="list">%s %s<span class="comma">,</span> %s<span class="comma">,<span/> <span class="hellip">...</span> %s</span>' % (
-        inline_brace('['),
-        atomic_type(s),
-        atomic_type(s),
-        inline_brace(']'),
-    )
+        if isinstance(data, Text):
+            return self.atomic_type(safe('string'))
 
-def choice_values(values):
-    return '<span class="choice">%s %s %s</span>' % (
-        inline_brace('('),
-        (' %s ' % inline_brace('|')).join(atomic_type(t) for t in values),
-        inline_brace(')'),
-    )
+        if isinstance(data, Positive):
+            return self.atomic_type(safe('positive integer'))
+
+        if isinstance(data, dict):
+            return self.format_hash(data, nested_hash, add_line_if_not_exist)
+
+        return self.block_comment('%s not supported' % safe(type(data)))
+
+    def format_hash(self, data, is_nested=False, add_line_if_not_exist=None):
+        lines = []
+        hash_items = data.items()
+        if add_line_if_not_exist:
+            hash_items.insert(0, add_line_if_not_exist)
+        is_last = lambda i, last_i=len(hash_items) - 1: i == last_i
+        for i, (key, value) in enumerate(sorted(hash_items,
+            key=lambda (key, value): (
+                isinstance(key, Optional),  # first non-optional
+                not isinstance(value, basestring), # next just strings
+                key,  # next abc-sorting
+            )
+        )):
+    #        if comment:
+    #            lines.append(block_comment(comment))
+            if isinstance(key, Optional):
+                lines.append(self.optional_line('%s %s%s' % (
+                    self.propname(safe(key.data) + ':'),
+                    self.format(value, nested_hash=True),
+                    '' if is_last(i) else self.comma()
+                )))
+            else:
+                lines.append(self.line('%s %s%s' % (
+                    self.propname(safe(key) + ':'),
+                    self.format(value, nested_hash=True),
+                    '' if is_last(i) else self.comma()
+                )))
+
+        core = ''.join(lines)
+        if core:
+            if is_nested:
+                return self.inline_brace('{') + self.indent(core) + self.block_brace('}')
+            else:
+                return self.block_brace('{') + self.indent(core) + self.block_brace('}')
+        else:
+            return self.inline_brace('{') + self.inline_brace('}')
+
+    def OR(self, lst, add_line_if_not_exist=None):
+        if len(lst) < 2:
+            return self.block(self.format(lst[0], add_line_if_not_exist=add_line_if_not_exist))
+        else:
+            return self.branching(lst)
 
 
-def block(s):
-    return '<div class="block">%s</div>' % s
+class DocstringFormatter(AbstractFormatter):
+    def comma(self):
+        return ','
 
-def line(s):
-    return '<div>%s</div>' % (s)
-
-def optional_line(s):
-    return '<div class="optional">%s &nbsp; %s</div>' % (s, line_comment('optional'))
-
-def block_comment(s):
-    return '<div class="comment">// %s</div>' % s
-
-def line_comment(s):
-    return '<span class="comment">// %s</span>' % s
-
-def propname(s):
-    return '<span class="property">%s</span>' % s
-
-def indent(*args):
-    return '<div class="indent">%s</div>' % ''.join(args)
-
-def format_hash(data, is_nested=False, add_line_if_not_exist=None):
-    lines = []
-    hash_items = data.items()
-    if add_line_if_not_exist:
-        hash_items.insert(0, add_line_if_not_exist)
-    is_last = lambda i, last_i=len(hash_items) - 1: i == last_i
-    for i, (key, value) in enumerate(sorted(hash_items,
-        key=lambda (key, value): (
-            isinstance(key, Optional),  # first non-optional
-            not isinstance(value, basestring), # next just strings
-            key,  # next abc-sorting
+    def branching(self, lst):
+        return (
+            '%s'
+        ) % ' | '.join(
+            '%s' % self.format(item) for item in lst
         )
-    )):
-#        if comment:
-#            lines.append(block_comment(comment))
-        if isinstance(key, Optional):
-            lines.append(optional_line('%s %s%s' % (
-                propname(safe(key.data) + ':'),
-                format(value, nested_hash=True),
-                '' if is_last(i) else '<span class="comma">,</span>'
-            )))
-        else:
-            lines.append(line('%s %s%s' % (
-                propname(safe(key) + ':'),
-                format(value, nested_hash=True),
-                '' if is_last(i) else '<span class="comma">,</span>'
-            )))
 
-    tail = indent(''.join(lines) or block_comment('empty')) + block_brace('}')
-    if is_nested:
-        return inline_brace('{') + tail
-    else:
-        return block_brace('{') + tail
+    def block_brace(self, s):
+        return '%s\n' % s
 
-def format(data, nested_hash=False, add_line_if_not_exist=None):
-    if isinstance(data, basestring):
-        return string(data)
-    if isinstance(data, AnyOf):
-        return OR(data.validators, add_line_if_not_exist)
-    if isinstance(data, type):
-        if data == bool:
-            return choice_values(['true', 'false'])
-        if data == int:
-            return atomic_type(safe('integer'))
-        else:
-            return atomic_type(safe('!!!! type %s !!!' % data))
-    if isinstance(data, list):
-        assert len(data) == 1
-        return list_type(format(data[0]))
-    if isinstance(data, Text):
-        return atomic_type(safe('string'))
-    if isinstance(data, Positive):
-        return atomic_type(safe('positive integer'))
-    if isinstance(data, dict):
-        #print 'data', data
-        return format_hash(data, nested_hash, add_line_if_not_exist)
-    return block_comment('%s not supported' % safe(type(data)))
-    #raise NotImplementedError('%s not supported' % type(data))
+    def inline_brace(self, s):
+        return s
 
-def safe(s):
-    return str(s).replace('<', '&lt;').replace('>', '&gt;')
+    def string(self, s):
+        return '"%s"' % s
 
-def OR(lst, add_line_if_not_exist=None):
-    if len(lst) < 2:
-        return block(format(lst[0], add_line_if_not_exist=add_line_if_not_exist))
-    else:
+    def atomic_type(self, s):
+        # hack
+        if s.startswith('<') and s.endswith('>'):
+            return s
+        return '<%s>' % s
+
+    def list_type(self, s):
+        return '%s %s, %s, ... %s' % (
+            self.inline_brace('['),
+            self.atomic_type(s),
+            self.atomic_type(s),
+            self.inline_brace(']'),
+        )
+
+    def choice_values(self, values):
+        return '%s %s %s\n' % (
+            self.inline_brace('('),
+            (' %s ' % self.inline_brace('|')).join(self.atomic_type(t) for t in values),
+            self.inline_brace(')'),
+        )
+
+    def block(self, s):
+        return '%s\n' % s
+
+    def line(self, s):
+        return '%s\n' % s
+
+    def optional_line(self, s):
+        return '%s  %s' % (s, self.line_comment('optional'))
+
+    def block_comment(self, s):
+        return '// %s\n' % s
+
+    def line_comment(self, s):
+        return '// %s\n' % s
+
+    def propname(self, s):
+        bits = s.split(':')
+        bits[0] = '"%s"' % bits[0]
+        return ':'.join(bits)
+
+    def indent(self, *args):
+        return '\n' + ''.join('    %s\n' % line for line in '\n'.join(args).split('\n'))
+
+class HtmlFormatter(DocstringFormatter):
+    def comma(self):
+        return '<span class="comma">,</span>'
+
+    def branching(self, lst):
         return (
             '<table class="branching">'
                 '<tr valign="top">'
                     '%s'
                 '</tr>'
-            '</table>') % (
-                '<td class="branch_splitter"></td>'.join(
-                    '<td class="branch">%s</td>' % format(item) for item in lst
-                )
-            )
+            '</table>'
+        ) % '<td class="branch_splitter"></td>'.join(
+            '<td class="branch">%s</td>' % self.format(item) for item in lst
+        )
+
+    def block_brace(self, s):
+        return '<div class="brace">%s</div>' % s
+
+    def inline_brace(self, s):
+        return '<span class="brace">%s</span>' % s
+
+    def string(self, s):
+        return '<span class="string">"%s"</span>' % s
+
+    def atomic_type(self, s):
+        return '<span class="variable">%s</span>' % s
+
+    def list_type(self, s):
+        return '<span class="list">%s %s<span class="comma">,</span> %s<span class="comma">,<span/> <span class="hellip">...</span> %s</span>' % (
+            self.inline_brace('['),
+            self.atomic_type(s),
+            self.atomic_type(s),
+            self.inline_brace(']'),
+        )
+
+    def choice_values(self, values):
+        return '<span class="choice">%s %s %s</span>' % (
+            self.inline_brace('('),
+            (' %s ' % self.inline_brace('|')).join(self.atomic_type(t) for t in values),
+            self.inline_brace(')'),
+        )
+
+    def block(self, s):
+        return '<div class="block">%s</div>' % s
+
+    def line(self, s):
+        return '<div>%s</div>' % (s)
+
+    def optional_line(self, s):
+        return '<div class="optional">%s &nbsp; %s</div>' % (s, self.line_comment('optional'))
+
+    def block_comment(self, s):
+        return '<div class="comment">// %s</div>' % s
+
+    def line_comment(self, s):
+        return '<span class="comment">// %s</span>' % s
+
+    def propname(self, s):
+        return '<span class="property">%s</span>' % s
+
+    def indent(self, *args):
+        return '<div class="indent">%s</div>' % ''.join(args)
+
+docstring_formatter = DocstringFormatter()
+html_formatter = HtmlFormatter()
+
+def safe(s):
+    return str(s).replace('<', '&lt;').replace('>', '&gt;')
 
 root = os.path.dirname(__file__)
 def get_file(fname):
@@ -148,9 +230,8 @@ class DocItem(object):
                 self.io_type = io_type
                 self.cleaned_name = cleaned_name
 
-#def mix_action_name():
 
-def generate_by_protocol(protocol, title='Untitled'):
+def generate_htmldoc_by_protocol(protocol, title='Untitled'):
     protocol = map(DocItem, protocol)
 
     requests =  [c for c in protocol if c.io_type == 'request']
@@ -180,9 +261,9 @@ def generate_by_protocol(protocol, title='Untitled'):
             'common_description': req.description,
             'req_id': req_id,
             'resp_id': resp_id,
-            'req_scheme': format(req.scheme,
+            'req_scheme': html_formatter.format(req.scheme,
                 add_line_if_not_exist=('action', req.cleaned_name)),
-            'resp_scheme': format(resp.scheme),
+            'resp_scheme': html_formatter.format(resp.scheme),
         })
 
     for c in other:
@@ -198,7 +279,7 @@ def generate_by_protocol(protocol, title='Untitled'):
         '</div>' % {
             'title': c.name,
             'description': c.description,
-            'scheme': format(c.scheme),
+            'scheme': html_formatter.format(c.scheme),
             'id': id,
         })
 
@@ -235,4 +316,3 @@ def generate_by_protocol(protocol, title='Untitled'):
             'ids':  repr(html_ids),  # js arrays syntax = python list syntax
         }
     )
-
