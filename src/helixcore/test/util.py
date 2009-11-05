8260355f632dea3faac51f129aab1b1cc51eb69f
Alexander Kislitsky <sand8080@gmail.com>
2009-11-05 13:04:59 +0300

    Common test functional moved to helixcore.test.util.py

---
+++ src/helixcore/test/util.py
@@ -0,0 +1,49 @@
+import datetime
+import cjson
+import urllib2
+import random
+
+
+def random_syllable(
+    consonant='r|t|p|l|k|ch|kr|ts|bz|dr|zh|g|f|d|s|z|x|b|n|m'.split('|'),
+    vowels='eyuioja'
+):
+    return ''.join(map(random.choice, (consonant, vowels)))
+
+
+def random_word(min_syllable=2, max_syllable=6):
+    return ''.join(random_syllable() for x in range(random.randint(min_syllable, max_syllable))) #@UnusedVariable
+
+
+def select_random(collection):
+    min_idx, max_idx = 0, len(collection) - 1
+    return collection[random.randint(min_idx, max_idx)]
+
+
+def profile(func):
+    def decorated(self, *args, **kwargs):
+        print '%s >>>>' % func.func_name
+        repeats = kwargs['repeats']
+        start = datetime.datetime.now()
+        for _ in xrange(repeats):
+            func(self, *args, **kwargs)
+        delta = datetime.datetime.now() - start
+        if delta.seconds == 0:
+            print 'repeats: %d, elapsed time %s' % (repeats, delta)
+        else:
+            print 'repeats: %d, elapsed time %s ~ [%s per second]' % (repeats, delta, repeats / delta.seconds)
+        print '%s <<<<' % func.func_name
+    return decorated
+
+
+class ClientApplication(object):
+    def __init__(self, host, port, login, password):
+        self.host = host
+        self.port = port
+        self.login = login
+        self.password = password
+
+    def request(self, data):
+        req = urllib2.Request(url='http://%s:%d' % (self.host, self.port), data=cjson.encode(data))
+        f = urllib2.urlopen(req)
+        return f.read()
