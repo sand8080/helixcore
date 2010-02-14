f4b66548f35066f6a2cc36a0e7984832eab75e23
Alexander Kislitsky <sand8080@gmail.com>
2010-02-15 00:01:37 +0300

    filter_all_field_values added to utils.

--- src/helixcore/test/test_utils.py
+++ src/helixcore/test/test_utils.py
@@ -9,6 +9,40 @@ class UtilTestCase(unittest.TestCase):
         self.assertEqual({}, utils.filter_dict([], {'b': 4, 'c': 5}))
         self.assertEqual({}, utils.filter_dict(('z'), {'b': 4, 'c': 5}))
 
+    def test_filter_all_field_values(self):
+        d = {
+            't': 'l0',
+            'some': None,
+            'nested': {
+                't': 'l1',
+                'abc': 2,
+                'def': {
+                    't': 'l2',
+                },
+            },
+            'no_t_dict': {'a': 1, 'b': 2},
+        }
+        self.assertEqual(sorted(('l0', 'l1', 'l2')), sorted(utils.filter_all_field_values('t', d)))
+        d = {
+            't': 'l0',
+            'details': [
+                {'t': 'l1-0'},
+                {'t': 'l1-1'},
+                {'b': 'c'},
+                [
+                    {'t': 'l2-0', 'n': 5},
+                    'b'
+                ],
+            ],
+            'tuple': ({'t': 'l1-2'}, 4, 5),
+        }
+        self.assertEqual(sorted(('l0', 'l1-0', 'l1-1', 'l1-2', 'l2-0')),
+            sorted(utils.filter_all_field_values('t', d)))
+
+        self.assertEqual((), utils.filter_all_field_values('t', 'siski'))
+        self.assertEqual((), utils.filter_all_field_values('t', None))
+        self.assertEqual((), utils.filter_all_field_values('t', 4))
+
 
 if __name__ == '__main__':
     unittest.main()
--- src/helixcore/utils.py
+++ src/helixcore/utils.py
@@ -11,3 +11,20 @@ def dict_from_lists(names, values):
 
 def filter_dict(e, d):
     return dict(filter(lambda x: x[0] in e, d.items()))
+
+
+def filter_all_field_values(f, d):
+    def is_container(o):
+        return isinstance(o, (dict, list, tuple))
+    val = ()
+    if not is_container(d):
+        return val
+    if isinstance(d, dict):
+        if f in d:
+            val = (d[f],)
+        nested = filter(is_container, d.values())
+    else:
+        nested = filter(is_container, d)
+    for n in nested:
+        val = val + filter_all_field_values(f, n)
+    return val
