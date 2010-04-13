315edd5df5ade6a59cf80bde2d9ce730919fe262
Alexander Kislitsky <sand8080@gmail.com>
2010-04-13 16:15:15 +0400

    Empty cond special case added. Code styling.

--- src/helixcore/db/buildhelpers.py
+++ src/helixcore/db/buildhelpers.py
@@ -4,21 +4,34 @@ class Unquoted(object):
     def __str__(self):
         return self.column
 
-def quote(val):
+
+def quote(val, quotechar='"', splitter='.'):
     if isinstance(val, Unquoted):
         return val.column
-    q, s = '"', '.'
-    return  s.join(x.startswith(q) and x.endswith(q) and x or '"%s"' % x for x in val.split(s))
+    return  splitter.join(
+        x.startswith(quotechar) and
+        x.endswith(quotechar) and
+        x or '%s%s%s' % (quotechar, x, quotechar)
+        for x in val.split(splitter)
+    )
+
 
 def quote_list(lst, separator=','):
     return separator.join(map(quote, lst))
 
+
+def is_empty_cond(cond):
+    cond_str, _ = cond.glue()
+    return cond_str == ''
+
+
 def where(cond):
-    if cond is None:
+    if cond is None or is_empty_cond(cond):
         return ('', [])
     else:
         cond_str, params = cond.glue()
         return ('WHERE %s' % cond_str, params)
+
 
 def order(order_by):
     if order_by is None:
@@ -32,6 +45,7 @@ def order(order_by):
         else:
             orders.append('%s ASC' % quote(o))
     return 'ORDER BY %s' % ','.join(orders)
+
 
 def columns(c):
     if c is None:
