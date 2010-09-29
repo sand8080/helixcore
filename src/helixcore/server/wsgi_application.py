6b42ae8eb3f449ed49ee4db4a5df29f7d036d5ed
Alexander Kislitsky <akislitsky@loyalty-partners.ru>
2010-09-29 18:27:43 +0400

    Wsgi application refactored
    Action tracing function changed, variables renamed, unclear code removed

--- src/helixcore/server/wsgi_application.py
+++ src/helixcore/server/wsgi_application.py
@@ -3,6 +3,7 @@ import logging
 import sys
 import traceback
 
+from helixcore import security
 from helixcore.server.api import Api as HelixApi
 from helixcore.server.response import response_error, response_app_error
 from helixcore.server.errors import RequestProcessingError
@@ -15,57 +16,68 @@ class Application(object):
         self.helix_api = HelixApi(protocol)
         self.tracking_api_calls = tracking_api_calls
 
-    def track_api_call(self, s_req, s_resp, remote_addr, authorized_data):
+    def track_api_call(self, s_req, s_resp, remote_addr,
+        action_name, authorized_data):
         pass
 
     def __call__(self, environ, start_response):
         raw_data = environ['eventlet.input'].read()
         remote_addr = environ.get('REMOTE_ADDR', 'undefined')
-        self.logger.debug('Request from %s' % remote_addr)
 
         action_name = None
-        data = {}
-        authorized_data = {}
-        response = None
+        processed_action_data = {}
+        secured_request = {}
+        secured_response = {}
+
         try:
-            action_name, data = self.helix_api.handle_request(raw_data)
-            self.logger.debug('Request from %s: %s' % (remote_addr, self.secured_request(action_name, data)))
-            authorized_data = dict(data)
-            raw_response = self.action_handler(action_name, authorized_data)
-            self.logger.log(logging.DEBUG, 'Response to %s: %s' % (remote_addr, raw_response))
+            action_name, action_data = self.helix_api.handle_request(raw_data)
+            secured_request = self._secured_request(action_name, action_data)
+            self.logger.debug('Request from %s: %s' % (remote_addr, secured_request))
+
+            processed_action_data = dict(action_data)
+            raw_response = self.action_handler(action_name, processed_action_data)
+
+            secured_response = security.sanitize_credentials(raw_response)
+            self.logger.log(logging.DEBUG, 'Response to %s: %s' % (remote_addr, secured_response))
+
             response = self.helix_api.handle_response(action_name, raw_response)
         except RequestProcessingError, e:
-            response = self.helix_api.handle_response(action_name, response_error(e), validation=False)
-            self.logger.log(logging.ERROR, 'Request from %s: %s' % (remote_addr, self.secured_request(action_name, data)))
-            self.logger.log(logging.ERROR, 'Response to %s: %s. Error: %s' % (remote_addr, response, e.message))
+            raw_response = response_error(e)
+            response = self.helix_api.handle_response(action_name, raw_response, validation=False)
+            self.logger.log(logging.ERROR, 'Request from %s: %s' % (remote_addr, secured_request))
+            secured_response = security.sanitize_credentials(raw_response)
+            self.logger.log(logging.ERROR, 'Response to %s: %s. Error: %s' % (remote_addr, secured_response, e.message))
         except Exception, e:
             exc_type, value, tb = sys.exc_info()
             exc_descr = 'Exception type: %s. message: %s. trace: %s' % (
                 exc_type, '; '.join(value.args), traceback.extract_tb(tb))
             del tb
+            raw_response = response_app_error(exc_descr)
             response = self.helix_api.handle_response(action_name,
-                response_app_error(exc_descr), validation=False)
+                raw_response, validation=False)
+            secured_response = security.sanitize_credentials(raw_response)
             self.logger.log(logging.ERROR, 'Response to %s: %s. General error: %s' %
-                (remote_addr, response, exc_descr))
+                (remote_addr, secured_response, exc_descr))
 
         start_response('200 OK', [('Content-type', 'text/plain')])
-        if action_name in self.tracking_api_calls:
-            request = cjson.encode(self.secured_request(action_name, data))
-            authorized_data.update(data) #has operator_id + login, password
-            secured_authorized_data = self.secured_request(action_name, authorized_data)
-            self.track_api_call(remote_addr, request, response, secured_authorized_data)
+
+        self._log_action(remote_addr, secured_request, secured_response,
+            action_name, processed_action_data)
+
         return [response]
 
+    def _log_action(self, remote_addr, secured_request, secured_response,
+        action_name, processed_action_data):
+        try:
+            if action_name in self.tracking_api_calls:
+                request = cjson.encode(secured_request)
+                response = cjson.encode(secured_response)
+                self.track_api_call(remote_addr, request,
+                    response, action_name, processed_action_data)
+        except Exception, e:
+            self.logger.log(logging.ERROR, 'Action logging failed: %s' % e)
 
-    def adapt(self, obj, req):
-        'Convert obj to bytes'
-        req.write(str(obj))
-
-    def secured_request(self, action_name, data):
-        d = {'action': action_name}
-        if data is not None:
-            d.update(data)
-        for f in ('password', 'new_password', 'su_password'):
-            if d.has_key(f):
-                d[f] = '******'
+    def _secured_request(self, action_name, data):
+        d = security.sanitize_credentials(data)
+        d['action'] = action_name
         return d
