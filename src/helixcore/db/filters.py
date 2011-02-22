1b63efccec13b5db385f79a9c072bdc7b17f0ed5
Alexander Kislitsky <sand8080@gmail.com>
2011-02-22 15:24:20 +0300

    OR handled in filter conditions map

--- src/helixcore/db/filters.py
+++ src/helixcore/db/filters.py
@@ -1,4 +1,5 @@
-from helixcore.db.sql import And, Any, NullLeaf, Select, Columns, AnyOf
+from helixcore.db.sql import And, Any, NullLeaf, Select, Columns, AnyOf, Or,\
+    Scoped
 from helixcore.db.wrapper import SelectedMoreThanOneRow, ObjectNotFound, fetchone_dict
 from helixcore import mapping
 
@@ -14,14 +15,29 @@ class ObjectsFilter(object):
         self.ordering_params = ordering_params if ordering_params else 'id'
         self.obj_class = obj_class
 
+    def _is_composed_parameter(self, p):
+        return isinstance(p, (list, tuple))
+
+    def _compose_cond(self, cond, p_value, db_f_name, c, composer):
+        if c in (Any, AnyOf):
+            return composer(cond, c(p_value, db_f_name))
+        else:
+            return composer(cond, c(db_f_name, p_value))
+
     def _cond_by_filter_params(self):
         cond = NullLeaf()
         for p_name, db_f_name, c in self.cond_map:
             if p_name in self.filter_params:
-                if c in (Any, AnyOf):
-                    cond = And(cond, c(self.filter_params[p_name], db_f_name))
+                if self._is_composed_parameter(p_name):
+                    cond_el = NullLeaf()
+                    p_value = self.filter_params[p_name]
+                    for p_value_el, db_f_name_el, c_el in zip(p_value, db_f_name, c):
+                        cond_el = self._compose_cond(cond_el, p_value_el,
+                            db_f_name_el, c_el, Or)
+                    cond = Scoped(cond_el)
                 else:
-                    cond = And(cond, c(db_f_name, self.filter_params[p_name]))
+                    cond = self._compose_cond(cond, self.filter_params[p_name],
+                        db_f_name, c, And)
         return cond
 
     def _get_paging_params(self):
