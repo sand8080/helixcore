b9b1c32def82f2987ef48282a7c6d7ff5247e86b
Alexander Kislitsky <sand8080@gmail.com>
2011-03-27 15:28:00 +0400

    Python package setup script implemented

---
+++ src/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+
+setup(
+    name = 'helixcore',
+    version = '1.0.0',
+    url = 'http://helixproject/',
+    author = 'Helixproject Developers',
+    author_email = 'developers@helixproject',
+    description = 'A core library of Helixproject',
+    download_url = 'http://helixproject/releases/helixcore/1.0.0/helixcore-1.0.0.tar.gz',
+    install_requires = ['python-cjson'],
+    classifiers = [
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.6',
+        'Operating System :: POSIX',
+        'Topic :: Software Development :: Libraries :: Application Frameworks',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Intended Audience :: Developers',
+        'Development Status :: 4 - Beta',
+    ],
+)
