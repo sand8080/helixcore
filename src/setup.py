62108edd5e51f7a12657253f0517cee9168d9da5
Alexander Kislitsky <sand8080@gmail.com>
2011-04-05 19:16:57 +0400

    Setup.py cleaned

--- src/setup.py
+++ src/setup.py
@@ -2,7 +2,6 @@ from setuptools import setup
 
 from helixcore.__version__ import version
 
-print '#### version', version
 
 setup(
     name = 'helixcore',
@@ -11,7 +10,6 @@ setup(
     author = 'Helixproject Developers',
     author_email = 'developers@helixproject',
     description = 'A core library of Helixproject',
-#    download_url = 'http://helixproject/releases/helixcore/1.0.0/helixcore-1.0.0.tar.gz',
     install_requires = ['python-cjson', 'iso8601', 'psycopg2', 'eventlet', 'pytz'],
     classifiers = [
         'License :: OSI Approved :: MIT License',
