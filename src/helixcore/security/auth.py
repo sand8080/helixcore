16c3405e38d21c6f4dbf537f11ea1bc69250bcc9
Alexander Kislitsky <akislitsky@loyalty-partners.ru>
2013-11-01 16:36:13 +0400

    Request passed into methods of CoreAuthenticator

--- src/helixcore/security/auth.py
+++ src/helixcore/security/auth.py
@@ -9,18 +9,17 @@ class CoreAuthenticator(object):
         self.login = partial(self._proxy_request, 'email')
         self.logout = partial(self._proxy_request, 'logout')
 
-    def check_access(self, session_id, service_type, property): #@ReservedAssignment
+    def check_access(self, req_info, session_id, service_type, property):  # @ReservedAssignment
         req = {'action': 'check_access', 'session_id': session_id,
-            'service_type': service_type, 'property': property}
-        return self.cli.request(req)
+               'service_type': service_type, 'property': property}
+        return self.cli.request(req, req_info)
 
-    def check_user_exist(self, session_id, user_id):
-        req = {'action': 'check_user_exist', 'session_id': session_id,
-            'id': user_id}
-        return self.cli.request(req)
+    def check_user_exist(self, req_info, session_id, user_id):
+        req = {'action': 'check_user_exist', 'session_id': session_id, 'id': user_id}
+        return self.cli.request(req, req_info)
 
-    def _proxy_request(self, action, data):
+    def _proxy_request(self, action, req_info, data):
         req = dict(data)
         req['action'] = action
-        return self.cli.request(req, check_response=False)
+        return self.cli.request(req, req_info, check_response=False)
 
