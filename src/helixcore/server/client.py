bade66a79c5ef06e712a0376deefb334ddadd045
Alexander Kislitsky <akislitsky@loyalty-partners.ru>
2013-11-01 16:35:31 +0400

    Request info handled in ip detection logic

--- src/helixcore/server/client.py
+++ src/helixcore/server/client.py
@@ -3,15 +3,16 @@ import urllib2
 
 from helixcore import error_code
 from helixcore.error import UnauthorizedActivity
+from helixcore.server.wsgi_application import RequestInfo
 
 
 class Client(object):
     def __init__(self, url):
         self.url = url
 
-    def _request(self, data, django_req):
+    def _request(self, data, request):
         try:
-            ip = self._client_ip(django_req)
+            ip = self._client_ip(request)
             h = {'X-Forwarded-For': ip}
             req = urllib2.Request(self.url, headers=h)
             f = urllib2.urlopen(req, json.dumps(data))
@@ -19,24 +20,27 @@ class Client(object):
             return json.loads(resp)
         except urllib2.URLError:
             return {'status': 'error', 'message': 'Service unavailable',
-                'code': error_code.HELIX_SERVICE_UNAVAILABLE}
+                    'code': error_code.HELIX_SERVICE_UNAVAILABLE}
 
-    def request(self, data, django_req, check_response=True):
-        resp = self._request(data, django_req)
+    def request(self, data, request, check_response=True):
+        resp = self._request(data, request)
         if check_response:
             self._check_response(resp)
         return resp
 
     def _check_response(self, resp):
         unauth = ('HELIXAUTH_SESSION_NOT_FOUND', 'HELIXAUTH_SESSION_EXPIRED',
-            'HELIXAUTH_USER_AUTH_ERROR')
+                  'HELIXAUTH_USER_AUTH_ERROR')
         if resp['status'] != 'ok' and resp['code'] in unauth:
             raise UnauthorizedActivity
 
     def _client_ip(self, request):
-        x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
-        if x_forwarded_for:
-            ip = x_forwarded_for.split(',')[0]
+        if isinstance(request, RequestInfo):
+            ip = request.remote_addr
         else:
-            ip = request.META.get('REMOTE_ADDR')
+            x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+            if x_forwarded_for:
+                ip = x_forwarded_for.split(',')[0]
+            else:
+                ip = request.META.get('REMOTE_ADDR')
         return ip
